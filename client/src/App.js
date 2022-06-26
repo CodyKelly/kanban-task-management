dcbdee131779ebc2e44328a94bad6feeb627b06c
CodyKelly <codykellydesign@gmail.com>
2022-06-26 14:50:32 -0600

    Added MUI

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Switch, createTheme } from '@mui/material';
+import { useState } from 'react';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+});
 
 function App() {
+  const [theme, setTheme] = useState(lightTheme);
+
+  const handleChange = () => {
+    setTheme((lastTheme) => {
+      return lastTheme === lightTheme ? darkTheme : lightTheme;
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      sx={{
+        width: '100vw',
+        height: '100vh',
+      }}
+    >
+      <Switch onChange={handleChange}></Switch>
+    </Box>
   );
 }
 
